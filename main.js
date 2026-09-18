2294ba3b8522b355f1947d1dfc2691d0a8d095ba
VictorLCM1227 <victor.luan.costa.matos@gmail.com>
2026-09-18 19:26:02 -0300

    aprendendo conceitos novos

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-let startBtn = document.getElementById('resetBtn')
+
+
+function start() {
+
+    let centesimos = 0
+    let segundos = 0
+    let minutos = 0
+
+    const milesimos = setInterval(() => {
+        centesimos++
+        if (centesimos == 100) {
+            centesimos = 0
+            segundos ++
+        }
+        if (segundos == 60) {
+            segundos = 0
+            minutos ++
+        }
+    },
+    100
+    )
+        
+    
+}
 
